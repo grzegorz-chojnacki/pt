650a35b36f78fca99a1a915ed9f08c75590ccb42
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2022-03-16 21:03:28 +0100

    Add file & directory deletion

--- app/MainWindow.xaml.cs
+++ app/MainWindow.xaml.cs
@@ -18,26 +18,47 @@ namespace app {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            var path = "C:\\Users\\User\\Desktop";
+            var root = TraverseTree(createTreeViewItem(path), path);
+            root.IsExpanded = true;
+            treeView.Items.Add(root);
         }
 
-        private TreeViewItem createTreeViewItemNode(string path) {
-            return new TreeViewItem
-            {
+        private TreeViewItem createTreeViewItem(string path) {
+            var node = new TreeViewItem {
                 Header = System.IO.Path.GetFileName(path),
-                Tag = path
+                Tag = path,
             };
+
+            var delete = new System.Windows.Controls.MenuItem() { Header = "Delete" };
+            delete.Click += (sender, e) => {
+                if (node.Parent is TreeViewItem) {
+                    ((TreeViewItem)node.Parent).Items.Remove(node);
+                } else {
+                    treeView.Items.Clear();
+                }
+
+                if (System.IO.File.Exists(path)) {
+                    System.IO.File.Delete(path);
+                } else if (System.IO.Directory.Exists(path)) {
+                    System.IO.Directory.Delete(path, true); // Delete recursively
+                }
+            };
+
+            node.ContextMenu = new System.Windows.Controls.ContextMenu() { Items = { delete }};
+            return node;
         }
 
         private TreeViewItem TraverseTree(TreeViewItem node, string path) {
             try {
                 foreach (var dirPath in System.IO.Directory.GetDirectories(path)) {
-                    var child = createTreeViewItemNode(dirPath);
+                    var child = createTreeViewItem(dirPath);
                     TraverseTree(child, dirPath);
                     node.Items.Add(child);
                 };
 
                 foreach (var filePath in System.IO.Directory.GetFiles(path)) {
-                    node.Items.Add(createTreeViewItemNode(filePath));
+                    node.Items.Add(createTreeViewItem(filePath));
                 };
             }
             catch (UnauthorizedAccessException) { /* Do nothing */ }
@@ -50,7 +71,7 @@ namespace app {
 
             if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
                 treeView.Items.Clear();
-                var root = TraverseTree(createTreeViewItemNode(dialog.SelectedPath), dialog.SelectedPath);
+                var root = TraverseTree(createTreeViewItem(dialog.SelectedPath), dialog.SelectedPath);
                 root.IsExpanded = true;
                 treeView.Items.Add(root);
             }
