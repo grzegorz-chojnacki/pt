fc3d99163b7235feaa2a627f54fb6af7bb6a04c5
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2022-03-18 21:27:43 +0100

    Code refactoring

--- app/MainWindow.xaml.cs
+++ app/MainWindow.xaml.cs
@@ -15,103 +15,129 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace app {
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-            var path = "C:\\Users\\User\\Desktop";
-            var root = TraverseTree(createTreeViewItem(path), path);
-            root.IsExpanded = true;
-            treeView.Items.Add(root);
-        }
+    public abstract class FileSystemTreeViewItem : TreeViewItem {
+        protected string Path;
 
-        private TreeViewItem createTreeViewItem(string path) {
-            var node = new TreeViewItem {
-                Header = System.IO.Path.GetFileName(path),
-                Tag = path,
-                ContextMenu = new System.Windows.Controls.ContextMenu(),
+        protected readonly System.Windows.Controls.MenuItem DeleteMenuItem = new System.Windows.Controls.MenuItem() { Header = "Delete" };
+        protected readonly System.Windows.Controls.MenuItem CreateMenuItem = new System.Windows.Controls.MenuItem() { Header = "Create" };
+        protected readonly System.Windows.Controls.MenuItem OpenMenuItem = new System.Windows.Controls.MenuItem() { Header = "Open" };
+
+        protected FileSystemTreeViewItem(string path) : base() {
+            Header = System.IO.Path.GetFileName(path);
+            Tag = path;
+            Path = path;
+            ContextMenu = new System.Windows.Controls.ContextMenu();
+
+            Selected += (sender, e) => {
+                SelectedHandler();
+                e.Handled = true; // Bubbling event workaround
             };
 
-            var delete = new System.Windows.Controls.MenuItem() { Header = "Delete" };
-            var create = new System.Windows.Controls.MenuItem() { Header = "Create" };
-            var open   = new System.Windows.Controls.MenuItem() { Header = "Open"   };
-
-            if (System.IO.File.Exists(path)) {
-                node.Selected += (sender, e) => {
-                    status.Text = System.IO.File.GetAttributes(path).ToString();
-                    e.Handled = true; // Bubbling event workaround
-                };
-
-                delete.Click += (sender, e) => {
-                    try {
-                        System.IO.File.Delete(path);
-                        ((TreeViewItem)node.Parent).Items.Remove(node);
-                    } catch (Exception ex) {
-                        Console.WriteLine(ex);
-                        System.Windows.MessageBox.Show("Something happened", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-                    }
-                };
-
-                if (System.IO.Path.GetExtension(path) == ".txt") {
-                    open.Click += (sender, e) => {
-                        using (var reader = System.IO.File.OpenText(path)) {
-                            fileView.Text = reader.ReadToEnd();
-                        }
-                    };
-
-                    node.ContextMenu.Items.Add(open);
+            DeleteMenuItem.Click += (sender, e) => {
+                try {
+                    DeleteHandler();
+                } catch (Exception ex) {
+                    Console.WriteLine(ex);
+                    System.Windows.MessageBox.Show("Something happened", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
                 }
+            };
 
-                node.ContextMenu.Items.Add(delete);
+            ContextMenu.Items.Add(DeleteMenuItem);
+        }
+
+        protected abstract void DeleteHandler();
+        protected abstract void SelectedHandler();
+
+        protected static TreeViewItem createTreeViewItem(string path) {
+            if (System.IO.File.Exists(path)) {
+                return new FileTreeViewItem(path);
             } else if (System.IO.Directory.Exists(path)) {
-                node.Selected += (sender, e) => {
-                    status.Text = new System.IO.DirectoryInfo(path).Attributes.ToString();
-                    e.Handled = true; // Bubbling event workaround
-                };
-
-                create.Click += (sender, e) => {
-                    var dialog = new CreateDialog() {
-                        Title = "Create file or directory",
-                        Owner = this,
-                        Path = path,
-                    };
-
-                    if (dialog.ShowDialog() == true) {
-                        node.Items.Insert(0, createTreeViewItem(dialog.fullPath));
-                    }
-                };
-
-                delete.Click += (sender, e) => {
-                    try {
-                        System.IO.Directory.Delete(path, true); // Delete recursively
-
-                        if (node.Parent is TreeViewItem item) {
-                            item.Items.Remove(node);
-                        } else {
-                            treeView.Items.Clear();
-                        }
-                    } catch (Exception ex) {
-                        Console.WriteLine(ex);
-                        System.Windows.MessageBox.Show("Something happened", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-                    }
-                };
-
-                node.ContextMenu.Items.Add(create);
-                node.ContextMenu.Items.Add(delete);
+                return new DirectoryTreeViewItem(path);
+            } else {
+                throw new Exception(path);
             }
+        }
+    }
 
-            return node;
+    public class FileTreeViewItem : FileSystemTreeViewItem {
+        protected override void DeleteHandler() {
+            System.IO.File.Delete(Path);
+            ((TreeViewItem)Parent).Items.Remove(this);
+        }
+        protected override void SelectedHandler() {
+            MainWindow.Instance.status.Text = System.IO.File.GetAttributes(Path).ToString();
+        }
+
+        public FileTreeViewItem(string path) : base(path) {
+            Selected += (sender, e) => {
+                e.Handled = true; // Bubbling event workaround
+            };
+
+            if (System.IO.Path.GetExtension(path) == ".txt") {
+                OpenMenuItem.Click += (sender, e) => {
+                    using (var reader = System.IO.File.OpenText(path)) {
+                        MainWindow.Instance.fileView.Text = reader.ReadToEnd();
+                    }
+                };
+
+                ContextMenu.Items.Insert(0, OpenMenuItem);
+            }
+        }
+    }
+
+    public class DirectoryTreeViewItem : FileSystemTreeViewItem {
+        protected override void SelectedHandler() {
+            MainWindow.Instance.status.Text = new System.IO.DirectoryInfo(Path).Attributes.ToString();
+        }
+
+        protected override void DeleteHandler() {
+            System.IO.Directory.Delete(Path, true); // Delete recursively
+
+            if (Parent is TreeViewItem item) {
+                item.Items.Remove(this);
+            } else {
+                MainWindow.Instance.treeView.Items.Clear();
+            }
+        }
+        public DirectoryTreeViewItem(string path) : base(path) {
+            CreateMenuItem.Click += (sender, e) => {
+                var dialog = new CreateDialog() {
+                    Title = "Create file or directory",
+                    Owner = MainWindow.Instance,
+                    Path = path,
+                };
+
+                if (dialog.ShowDialog() == true) {
+                    Items.Insert(0, createTreeViewItem(dialog.fullPath));
+                }
+            };
+
+            ContextMenu.Items.Insert(0, CreateMenuItem);
+        }
+    }
+
+    public partial class MainWindow : Window {
+        public static MainWindow Instance;
+        public MainWindow() {
+            InitializeComponent();
+            Instance = this;
+
+            var path = "C:\\Users\\User\\Desktop";
+            var root = TraverseTree(new DirectoryTreeViewItem(path), path);
+            root.IsExpanded = true;
+            treeView.Items.Add(root);
         }
 
         private TreeViewItem TraverseTree(TreeViewItem node, string path) {
             try {
                 foreach (var dirPath in System.IO.Directory.GetDirectories(path)) {
-                    var child = createTreeViewItem(dirPath);
+                    var child = new DirectoryTreeViewItem(dirPath);
                     TraverseTree(child, dirPath);
                     node.Items.Add(child);
                 };
 
                 foreach (var filePath in System.IO.Directory.GetFiles(path)) {
-                    node.Items.Add(createTreeViewItem(filePath));
+                    node.Items.Add(new FileTreeViewItem(filePath));
                 };
             } catch (UnauthorizedAccessException) { /* Do nothing */ }
 
@@ -123,7 +149,7 @@ namespace app {
 
             if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
                 treeView.Items.Clear();
-                var root = TraverseTree(createTreeViewItem(dialog.SelectedPath), dialog.SelectedPath);
+                var root = TraverseTree(new DirectoryTreeViewItem(dialog.SelectedPath), dialog.SelectedPath);
                 root.IsExpanded = true;
                 treeView.Items.Add(root);
             }
