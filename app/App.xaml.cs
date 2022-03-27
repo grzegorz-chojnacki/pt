8656ff364bb37c3788b10d3789a1b78b38c4e80b
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2022-03-27 18:09:36 +0200

    Formatting

--- app/App.xaml.cs
+++ app/App.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace app
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace app {
+    public partial class App : Application { }
 }
