bcf72f8e79974dcbf255ad2966a6a2fb6b034a30
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2022-03-27 19:06:52 +0200

    Refactored project structure

--- app/Windows/FileSystemTreeViewItem.cs
+++ app/Windows/FileSystemTreeViewItem.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 using ControlMenuItem = System.Windows.Controls.MenuItem;
 
-namespace app {
+namespace app.Windows {
     public abstract class FileSystemTreeViewItem : TreeViewItem {
         protected string RootPath;
 
@@ -111,57 +111,6 @@ namespace app {
             };
 
             ContextMenu.Items.Insert(0, CreateMenuItem);
-        }
-    }
-
-    public partial class MainWindow : Window {
-        public static MainWindow Instance;
-        public MainWindow() {
-            InitializeComponent();
-            Instance = this;
-
-            var path = "C:\\Users\\User\\Desktop";
-            var root = TraverseTree(new DirectoryTreeViewItem(path), path);
-            root.IsExpanded = true;
-            treeView.Items.Add(root);
-        }
-
-        private TreeViewItem TraverseTree(TreeViewItem node, string path) {
-            try {
-                foreach (var dirPath in Directory.GetDirectories(path)) {
-                    var child = new DirectoryTreeViewItem(dirPath);
-                    TraverseTree(child, dirPath);
-                    node.Items.Add(child);
-                };
-
-                foreach (var filePath in Directory.GetFiles(path)) {
-                    node.Items.Add(new FileTreeViewItem(filePath));
-                };
-            } catch (UnauthorizedAccessException) { /* Do nothing */ }
-
-            return node;
-        }
-
-        public void SetAttributes(FileAttributes attributes) {
-            MainWindow.Instance.status.Text = (((attributes & FileAttributes.ReadOnly) != 0) ? "r" : "-")
-                    + (((attributes & FileAttributes.Archive) != 0) ? "a" : "-")
-                    + (((attributes & FileAttributes.System) != 0) ? "s" : "-")
-                    + (((attributes & FileAttributes.Hidden) != 0) ? "h" : "-");
-        }
-
-        private void OpenDirectory(object sender, RoutedEventArgs e) {
-            var dialog = new FolderBrowserDialog() { Description = "Select directory to open" };
-
-            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
-                treeView.Items.Clear();
-                var root = TraverseTree(new DirectoryTreeViewItem(dialog.SelectedPath), dialog.SelectedPath);
-                root.IsExpanded = true;
-                treeView.Items.Add(root);
-            }
-        }
-
-        private void Exit(object sender, RoutedEventArgs e) {
-            System.Windows.Forms.Application.Exit();
         }
     }
 }
