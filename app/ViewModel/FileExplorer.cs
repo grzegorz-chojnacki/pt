ed0d3a86226324f37729282b02e07dbfdfd1559c
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2022-04-04 21:22:19 +0200

    Add language buttons

--- app/ViewModel/FileExplorer.cs
+++ app/ViewModel/FileExplorer.cs
@@ -1,6 +1,24 @@
-﻿namespace app.ViewModel {
+﻿using System.Globalization;
+
+namespace app.ViewModel {
     public class FileExplorer : ViewModelBase {
+        public string Lang {
+            get { return CultureInfo.CurrentUICulture.TwoLetterISOLanguageName; }
+            set {
+                if (value != null)
+                    if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName != value) {
+                        CultureInfo.CurrentUICulture = new CultureInfo(value);
+                        NotifyPropertyChanged();
+                    }
+            }
+        }
+
         public DirectoryInfoViewModel Root { get; set; }
+
+        public FileExplorer() {
+            NotifyPropertyChanged(nameof(Lang));
+        }
+
         public void OpenRoot(string path) {
             Root = new DirectoryInfoViewModel();
             Root.Open(path);
---
+++ app/ViewModel/LangBoolConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace app.ViewModel {
+    public class LangBoolConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return ((string)value == (string)parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return ((bool)value) ? (string)parameter : null;
+        }
+    }
+}
