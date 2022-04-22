13568648685b1f8d7ca43b99873ea014cde0a75f
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2022-04-22 19:16:30 +0200

    Add CanExecute param to RelayCommand implementation

--- app/ViewModel/RelayCommand.cs
+++ app/ViewModel/RelayCommand.cs
@@ -4,23 +4,24 @@ using System.Windows.Input;
 namespace app.ViewModel {
     public class RelayCommand : ICommand {
         Action<object> _execute;
-
+        Predicate<object> _canExecute = _ => true;
 
         public RelayCommand(Action<object> execute) {
             _execute = execute;
         }
 
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute) {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
         public bool CanExecute(object parameter) {
-           return true;
+           return _canExecute(parameter);
         }
 
         public event EventHandler CanExecuteChanged {
-            add {
-                CommandManager.RequerySuggested += value;
-            }
-            remove {
-                CommandManager.RequerySuggested -= value;
-            }
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
         }
 
         public void Execute(object parameter) {
