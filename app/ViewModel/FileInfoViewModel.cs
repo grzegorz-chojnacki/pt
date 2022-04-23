bf993146397701249a98de5782bd8322a2d86a99
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2022-04-23 21:46:01 +0200

    Add extension constraints to OpenFileCommand

--- app/ViewModel/FileInfoViewModel.cs
+++ app/ViewModel/FileInfoViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System.IO;
+using System.Linq;
 using System.Windows.Input;
 
 namespace app.ViewModel {
     public class FileInfoViewModel : FileSystemInfoViewModel {
+        public static readonly string[] TextFilesExtensions = new string[] { ".txt", ".ini", ".log" };
 
         public string Icon {
             get => (Model.Extension == ".txt") ? "/Resources/txt.png" : "/Resources/unknown.png";
@@ -11,13 +13,13 @@ namespace app.ViewModel {
         public ICommand OpenCommand { get; }
 
         public FileInfoViewModel(FileExplorer owner) : base(owner) {
-            OpenCommand = new RelayCommand(param => {
+            OpenCommand = new RelayCommand(_ => {
                 if (Model.Extension == ".txt") {
                     using (var reader = File.OpenText(Model.FullName)) {
                         owner.Window.fileView.Text = reader.ReadToEnd();
                     }
                 };
-            });
+            }, _ => TextFilesExtensions.Contains(Model.Extension));
         }
 
         protected override void DeleteHandler() => Model.Delete();
